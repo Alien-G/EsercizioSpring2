cb53f40732182b8087ca2b547613ec992f689051
GA <gaia.addeo@gmail.com>
2023-07-24 14:38:19 +0200

    Exercise 2

---
+++ SpringInitializer/src/main/java/Service/Meal.java
@@ -0,0 +1,48 @@
+package Service;
+
+public class Meal {
+private String name;
+private Double rating;
+private Double price;
+
+private String description;
+
+    public Meal(String name, Double price, Double rating, String description) {
+        this.name = name;
+        this.rating = rating;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
